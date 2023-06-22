04c76337f9b53d4dc3e2b2bcdbec177b33aacca7
sikiru yaya <sikiruyaya1@gmail.com>
2023-06-22 09:43:17 +0000

    closes the session

--- models/state.py
+++ models/state.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
 """This is the state class"""
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.orm import relationship, backref
-import models
-from os import environ
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel, Base):
     """This is the class for State
+
     Attributes:
         name: input name
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City",
-                              backref="state",
-                              cascade="all, delete, delete-orphan")
+    """
+
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
     else:
+        name = ''
 
         @property
         def cities(self):
-            """ Returns the list of City instances with
-            state_id == current State.id """
-            all_cities = models.storage.all(City)
-            state_cities = []
-            for city_ins in all_cities.values():
-                if city_ins.state_id == self.id:
-                    state_cities.append(city_ins)
+            """Returns the list of `City` instances
+            with `state_id` equals to the current
+            """
 
-            return state_cities
+            cities = list()
+
+            for _id, city in models.storage.all(City).items():
+                if city.state_id == self.id:
+                    cities.append(city)
+
+            return (cities)

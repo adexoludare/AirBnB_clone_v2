a2d008fb977fb99ef8d65bc334a49fa269f4c3f6
sikiru yaya <sikiruyaya1@gmail.com>
2023-06-22 09:46:49 +0000

    Modules import issues

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""create a unique FileStorage or DbStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+"""create a unique FileStorage instance for your application"""
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -9,13 +7,13 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from os import environ
+from os import getenv
 
-sqlStorage = environ.get('HBNB_TYPE_STORAGE')
-
-if sqlStorage == "db":
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()

d0442f187a491bf40337cce94809544e36646b1d
adexoludare <adexoludare@gmail.com>
2023-07-07 12:59:53 +0100

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,11 @@ def do_pack():
     of the web_static folder of this repository.
     """
 
-    d = datetime.now()
-    now = d.strftime('%Y%m%d%H%M%S')
-
-    local("mkdir -p versions")
-    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
